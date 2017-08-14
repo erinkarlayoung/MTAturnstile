1f45e6fe1aa7d196e1bc97df85e72a18a9b0d130
ErinKarlaYoung <erinkarlayoung@gmail.com>
2017-08-14 18:04:30 -0400

    seriously Nick turn off that fucking music

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808200358) do
+ActiveRecord::Schema.define(version: 20170814194824) do
+
+  create_table "homes", force: :cascade do |t|
+    t.string "create"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ratings", force: :cascade do |t|
     t.integer "station_id"
